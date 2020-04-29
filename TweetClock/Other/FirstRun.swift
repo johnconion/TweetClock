068f262c43c2765e74993cd78becae4278a12660
johnconion <zyonnkonionn@gmail.com>
2020-04-29 09:59:20 +0900

    初期の背景色を更新

--- TweetClock/Other/FirstRun.swift
+++ TweetClock/Other/FirstRun.swift
@@ -17,8 +17,8 @@ class FirstRun {
         UserDefaultManager.setValue(key: .TextGreenColor, value: Float(1.0))
         UserDefaultManager.setValue(key: .TextBlueColor, value: Float(1.0))
         UserDefaultManager.setValue(key: .BackgroundRedColor, value: Float(0.0))
-        UserDefaultManager.setValue(key: .BackgroundGreenColor, value: Float(0.2))
-        UserDefaultManager.setValue(key: .BackgroundBlueColor, value: Float(0.35))
+        UserDefaultManager.setValue(key: .BackgroundGreenColor, value: Float(0.3))
+        UserDefaultManager.setValue(key: .BackgroundBlueColor, value: Float(0.45))
         UserDefaultManager.setValue(key: .LoadTimelineInterval,value: 1.0)
     }
 }
